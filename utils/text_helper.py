87d659e43f18db8312ce26d3420cc1787cc4d6ab
Danny Gerst <d.gerst@bizrock.de>
2024-03-09 16:00:24 +0100

    Added a method to replace all numbers with emojie numbers in a given text

--- utils/text_helper.py
+++ utils/text_helper.py
@@ -8,3 +8,23 @@ def escape_html(text):
             .replace("'", '&#39;'))
     else:
         return text
+
+
+def replace_numbers_with_emojis(text):
+    # Emoji-Zuordnungen für Zahlen 0 bis 9
+    emoji_map = {
+        '0': '0️⃣',
+        '1': '1️⃣',
+        '2': '2️⃣',
+        '3': '3️⃣',
+        '4': '4️⃣',
+        '5': '5️⃣',
+        '6': '6️⃣',
+        '7': '7️⃣',
+        '8': '8️⃣',
+        '9': '9️⃣'
+    }
+    # Ersetze jede Zahl im Text durch das entsprechende Emoji
+    for number, emoji in emoji_map.items():
+        text = text.replace(number, emoji)
+    return text
